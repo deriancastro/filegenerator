efbb03f6eff8dcf5da9bdd5edd316054725b0cf8
Derian Castro <deriancastro@hotmail.com>
2021-05-26 13:05:48 +0200

    Add inquirer Promp

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const writeFile = require('./writeFile')
-const functionName = process.argv[2] ?? 'myfunction'
+const inquirer = require('inquirer')
 
-writeFile(functionName)
-
-// console.log(process.argv)
-/*
-[
-    '/opt/homebrew/lib/node_modules/node/bin/node',
-    '/Users/macfrei/neuefische/coaches/filegenerator/index.js',
-    'multiply'
+const questions = [
+  {
+    type: 'input',
+    name: 'file',
+    message:
+      'Which functions and files would you like to create? Please separete by comma. ',
+  },
 ]
-*/
+
+inquirer.prompt(questions).then(answers => {
+  //answer is an object: {file: 'header, footer'}
+  const files = answers['file'].split(',').map(el => el.trim())
+  files.forEach(file => writeFile(file))
+  console.log(files)
+})
