c8707520c748151c3b5973907f05b44ee3069209
Derian Castro <deriancastro@hotmail.com>
2021-05-26 17:07:54 +0200

    Fix more bugs

--- index.js
+++ index.js
@@ -41,8 +41,7 @@ const questions = [
   {
     type: 'input',
     name: 'name',
-    message:
-      'Which functions and files would you like to create? Plase seperate by comma.',
+    message: 'Which function and file would you like to create? ',
   },
   {
     type: 'checkbox',
