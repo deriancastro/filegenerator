7f8ea96ffec945144b10751119ef6b34d2f634df
Derian Castro <deriancastro@hotmail.com>
2021-05-26 11:34:41 +0200

    Add Promp v1

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const writeFile = require('./writeFile')
-const functionName = process.argv[2] ?? 'myfunction'
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+rl.question('Hello, how are you', answer => {
+  console.log(answer)
+  rl.close()
+})
+
+/*const functionName = process.argv[2] ?? 'myfunction'
 
 writeFile(functionName)
 
 // console.log(process.argv)
-/*
+
 [
     '/opt/homebrew/lib/node_modules/node/bin/node',
     '/Users/macfrei/neuefische/coaches/filegenerator/index.js',
